9a97e904ea47fa6ed0aa1bf4e552a2335d53c57e
rahmatullah18 <rahmatullahsmakenza@gmail.com>
2022-11-26 08:20:15 +0800

    update ui

--- components/content/Content.js
+++ components/content/Content.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import Wave from 'react-wavify'
+import React from 'react'
 import Pagination from '../Pagination'
 import About from './About'
 import ButtonContent from './ButtonContent'
@@ -7,9 +6,8 @@ import Contact from './Contact'
 import Title from './Title'
 
 export default function Content() {
-  const [waveHeight, setWaveHeight] = useState(false)
   return (
-    <div className='relative flex flex-col' onMouseEnter={() => setWaveHeight(true)} onMouseLeave={() => setWaveHeight(false)}>
+    <div className='relative flex flex-col'>
       <div className='z-10 mx-5 my-10 space-y-10 md:mx-10' >
         {/* title */}
         <Title />
@@ -21,23 +19,6 @@ export default function Content() {
         <Contact />
         <Pagination />
       </div>
-      <Wave className={`absolute bottom-0 transition delay-75 ease-in duration-500 ${waveHeight ? '-translate-y-16' : ""}`} mask="url(#mask)" fill={'#1277b0'}
-        options={{
-          height: 20,
-          amplitude: 20,
-          speed: 0.15,
-          points: 5
-        }}>
-        <defs>
-          <linearGradient id="gradient" gradientTransform="rotate(90)">
-            <stop offset="0" stopColor="white" />
-            <stop offset="0.5" stopColor="black" />
-          </linearGradient>
-          <mask id="mask">
-            <rect x="0" y="0" width="2000" height="300" fill="url(#gradient)" />
-          </mask>
-        </defs>
-      </Wave>
     </div>
   )
 }
--- components/content/Title.js
+++ components/content/Title.js
@@ -1,3 +1,4 @@
+import { TypeAnimation } from 'react-type-animation'
 import { ArrowDownCircleIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
 import React from 'react'
@@ -6,12 +7,29 @@ export default function Title() {
   return (
     <div className='flex-col space-y-3'>
       <div className='flex items-center justify-between space-x-2'>
-        <h1 className='text-5xl font-extrabold drop-shadow-md'>Hi! I Am </h1>
+        <h1 className='text-5xl font-extrabold drop-shadow-md'>Hi, I am</h1>
         <Link href={'https://www.cakeresume.com/rahmatullah-ashar'}>
           <a className='flex items-center gap-2 px-4 py-2 text-lg font-normal text-white rounded-full bg-purple-figma' target={'_blank'}><ArrowDownCircleIcon className='w-7 h-7' /> Resume </a></Link>
       </div>
       <div className='flex items-center justify-start'>
-        <h1 className='text-4xl font-extrabold tracking-wide drop-shadow-md'>Rahmatullah Ashar</h1>
+        {/* <h1 className='text-4xl font-extrabold tracking-wide drop-shadow-md'>Rahmatullah Ashar</h1> */}
+        <TypeAnimation
+          sequence={[
+            'Rahmatullah Ashar', // Types 'One'
+            4000, // Waits 1s
+            'Web Developer', // Deletes 'One' and types 'Two'
+            3000, // Waits 2s
+            'Freshgraduate', // Types 'Three' without deleting 'Two'
+            1000,
+            () => {
+              console.log('Done typing!'); // Place optional callbacks anywhere in the array
+            }
+          ]}
+          wrapper="div"
+          cursor={true}
+          repeat={Infinity}
+          style={{ fontSize: '2em', fontWeight: 700 }}
+        />
       </div>
     </div>
   )
